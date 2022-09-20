9b064d04b582f0964b895623cf14f49d14c5e327
Fehlpe <felpearcanjo@gmail.com>
2022-09-20 15:51:33 -0300

    att: atv-final quase pronta

--- PRACTICES/FRONT-END-2/atividade-Final/src/home.ts
+++ PRACTICES/FRONT-END-2/atividade-Final/src/home.ts
@@ -1,78 +1,116 @@
-"use strict";
 let loggedHome = sessionStorage.getItem('logged');
-const userList = JSON.parse(localStorage.getItem('users') || '[]');
-const btnForm = document.querySelector("#novo-recado-submit");
-const newNoteTitleInput = document.querySelector("#titulo-novo-recado-input");
-const newNoteInput = document.querySelector("#novo-recado-input");
-const elementList = document.querySelector("#recados");
-const btnLogout = document.getElementById("logout");
-const titleWithName = document.getElementById("titulo-home");
-document.addEventListener('DOMContentLoaded', () => {
-    if (!loggedHome) {
+const userList = JSON.parse(localStorage.getItem('users') || '[]'); 
+const btnForm = document.querySelector("#novo-recado-submit") as HTMLButtonElement;
+const newNoteTitleInput = document.querySelector("#titulo-novo-recado-input") as HTMLInputElement;
+const newNoteInput = document.querySelector("#novo-recado-input") as HTMLInputElement;
+const elementList = document.querySelector("#recados") as HTMLDivElement; 
+const btnLogout = document.getElementById("logout") as HTMLButtonElement;
+const titleWithName = document.getElementById("titulo-home") as HTMLHeadingElement;
+
+interface Note{
+    title: string,
+    description: string,
+    id: number
+}
+
+interface User{
+    username:string,
+    email: string,
+    password: string,
+    notes: Note[]
+}
+
+document.addEventListener('DOMContentLoaded', () => {   
+    if(!loggedHome) {
         window.location.href = "index.html";
         return;
     }
+
     let userList = searchAllUsers();
-    let user = userList.find((valor) => valor.email == loggedHome);
-    titleWithName.innerHTML = `Olá, <span>${user.username}</span> `;
-    user.notes.forEach((recado) => montarRecado(recado));
-});
-function searchAllUsers() {
-    return JSON.parse(localStorage.getItem('users') || '[]');
+
+    let user = userList.find(
+        (valor) => valor.email == loggedHome) as User;
+    
+    titleWithName.innerHTML = `Olá, <span>${user.username}</span> `;    
+
+    user.notes.forEach((recado: Note) => montarRecado(recado))
+
+})
+
+function searchAllUsers(): User[]{
+  return JSON.parse(localStorage.getItem('users') || '[]');
 }
+
 btnLogout.addEventListener('click', () => {
-    sessionStorage.removeItem('logged');
+    sessionStorage.removeItem('logged')
     document.location.reload();
-});
+})
+
 btnForm.addEventListener('click', (e) => {
     e.preventDefault();
-    if (newNoteInput.value == '' || newNoteTitleInput.value == '') {
+
+    if(newNoteInput.value=='' || newNoteTitleInput.value==''){
         alert("Preencha todos os campos do novo recado!");
-        return;
+        return
     }
+
     salvarRecado();
+
 });
-function salvarRecado() {
+
+function salvarRecado(): void{
     let recado = newNoteInput.value;
     let tituloRecado = newNoteTitleInput.value;
-    const novoRecado = {
+
+    const novoRecado: Note = {
         title: tituloRecado,
         description: recado,
         id: Math.floor(Math.random() * Date.now())
-    };
-    let user = findUser();
-    user.notes.push(novoRecado);
+    }
+
+    let user = findUser()
+
+    user.notes.push(novoRecado);    
+    
     atualizarDadosUsuario(user);
     montarRecado(novoRecado);
 }
-function findUser() {
+
+function findUser(): User{
+
     let userList = searchAllUsers();
-    return userList.find((valor) => valor.email == loggedHome);
+    return userList.find((valor) => valor.email == loggedHome) as User;
 }
-function atualizarDadosUsuario(dadosAtualizados) {
-    let userList = searchAllUsers();
+
+function atualizarDadosUsuario(dadosAtualizados: User){
+
+    let userList = searchAllUsers()
     let indiceUsuarioEncontrado = userList.findIndex((valor) => valor.email === dadosAtualizados.email);
+
     userList[indiceUsuarioEncontrado] = dadosAtualizados;
+
     atualizarStorage(userList);
 }
-function atualizarStorage(userList) {
+
+function atualizarStorage(userList:User[]): void{
     localStorage.setItem('users', JSON.stringify(userList));
 }
-function montarRecado(novoRecado) {
+
+function montarRecado(novoRecado: Note){  
     let titulo = novoRecado.title;
-    let recado = novoRecado.description;
+    let recado =  novoRecado.description;
     const elementoRecado = document.createElement("div");
     elementoRecado.classList.add("recado", "p-2", "mb-2", "mt-3", "d-flex", "flex-row");
     elementoRecado.setAttribute('id', `${novoRecado.id}`);
     const conteudoRecado = document.createElement("div");
     conteudoRecado.classList.add("conteudo-recado", "d-flex", "flex-row");
     elementoRecado.appendChild(conteudoRecado);
-    const inputRecado = document.createElement("textarea");
+    const inputRecado = document.createElement("textarea") as HTMLTextAreaElement;
     inputRecado.classList.add("texto-recado");
     inputRecado.value = recado;
     inputRecado.setAttribute("readonly", "readonly");
     inputRecado.setAttribute("oninput", "auto_height(this)");
-    const inputTitulo = document.createElement("textarea");
+    const inputTitulo = document.createElement("textarea") as HTMLTextAreaElement;
     inputTitulo.classList.add("titulo-recado");
     inputTitulo.value = titulo;
     inputTitulo.setAttribute("readonly", "readonly");
@@ -81,7 +119,7 @@ function montarRecado(novoRecado) {
     conteudoRecado.appendChild(inputRecado);
     const botoesRecado = document.createElement("div");
     botoesRecado.classList.add("botoes-recado", "d-flex", "justify-content-between", "flex-row");
-    const botaoEditarRecado = document.createElement("button");
+    const botaoEditarRecado = document.createElement("button") as HTMLButtonElement;
     botaoEditarRecado.classList.add("editar-recado");
     botaoEditarRecado.innerHTML = "EDITAR";
     const botaoDeletarRecado = document.createElement("button");
@@ -91,21 +129,20 @@ function montarRecado(novoRecado) {
     botoesRecado.appendChild(botaoDeletarRecado);
     elementoRecado.appendChild(botoesRecado);
     elementList.appendChild(elementoRecado);
-    botaoEditarRecado.addEventListener('click', () => editarRecado(botaoEditarRecado, inputTitulo, inputRecado, novoRecado.id));
+    botaoEditarRecado.addEventListener('click', () => editarRecado(botaoEditarRecado,inputTitulo, inputRecado, novoRecado.id));
     botaoDeletarRecado.addEventListener('click', () => apagarRecado(novoRecado.id));
-}
-;
-function editarRecado(botaoEditarRecado, inputTitulo, inputRecado, id) {
+};
+
+function editarRecado(botaoEditarRecado: HTMLButtonElement,inputTitulo: HTMLTextAreaElement, inputRecado: HTMLTextAreaElement, id: number){
     let user = findUser();
     let recadoEspecifico = user.notes.findIndex((recado) => recado.id === id);
-    if (botaoEditarRecado.innerText.toLocaleLowerCase() == "editar") {
+    if(botaoEditarRecado.innerText.toLocaleLowerCase() == "editar"){
         inputRecado.removeAttribute("readonly");
         inputRecado.focus();
         inputTitulo.removeAttribute("readonly");
         inputTitulo.focus();
         botaoEditarRecado.innerText = "SALVAR";
-    }
-    else {
+    } else {
         inputRecado.setAttribute("readonly", "readonly");
         inputTitulo.setAttribute("readonly", "readonly");
         botaoEditarRecado.innerText = "EDITAR";
@@ -116,21 +153,25 @@ function editarRecado(botaoEditarRecado, inputTitulo, inputRecado, id) {
         atualizarDadosUsuario(user);
     }
 }
-function apagarRecado(id) {
+
+function apagarRecado(id:number){
     let user = findUser();
     let recadoEspecifico = user.notes.findIndex((recado) => recado.id === id);
-    let linhaRecado = document.getElementById(`${id}`);
+
+    let linhaRecado = document.getElementById(`${id}`) as HTMLDivElement;
+
     let confirma = confirm(`Você realmente deseja apagar esse recado?`);
-    if (confirma) {
+
+    if(confirma){
         linhaRecado.remove();
         user.notes.splice(recadoEspecifico, 1);
         atualizarDadosUsuario(user);
-    }
-    else {
-        return;
+    } else {
+        return
     }
 }
-function auto_height(elem) {
+
+function auto_height(elem:any) {  /* javascript */
     elem.style.height = "1px";
-    elem.style.height = (elem.scrollHeight) + "px";
+    elem.style.height = (elem.scrollHeight)+"px";
 }
