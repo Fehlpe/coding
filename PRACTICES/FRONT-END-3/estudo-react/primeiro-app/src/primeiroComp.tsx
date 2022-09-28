1572c7aa9915020a1dd897a3942077ff67f338ab
Fehlpe <felpearcanjo@gmail.com>
2022-09-27 21:07:03 -0300

    começo real react

---
+++ PRACTICES/FRONT-END-3/estudo-react/primeiro-app/src/primeiroComp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PrimeiroComponent() {
+    return (
+        <div>
+            <h1>Meu Primeiro Component</h1>
+        </div>
+    )
+}
+
+export default PrimeiroComponent;
