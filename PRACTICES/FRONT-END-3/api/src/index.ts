1345c75fd078380b004b4d1eaee53218a1651b3f
Fehlpe <felpearcanjo@gmail.com>
2022-11-23 00:06:57 -0300

    typo

--- PRACTICES/FRONT-END-3/api/src/index.ts
+++ PRACTICES/FRONT-END-3/api/src/index.ts
@@ -9,3 +9,29 @@ app.listen(3000, () => {
 app.get("/", (req: Request, res: Response) => {
     res.send("Oss")
 });
+
+interface Produto {
+    id:number;
+    descricao:string;
+    preco:number;
+    disponivel:boolean;
+}
+
+const lista: Produto[] = [];
+
+app.use(express.json());
+
+app.post("/produtos", (req: Request, res: Response) => {
+    const { descricao, preco, disponivel } = req.body
+    
+    const novo = {
+        id: lista.length + 1,
+        descricao,
+        preco,
+        disponivel,
+    };
+
+    lista.push(novo);
+    res.json(novo);
+
+})
